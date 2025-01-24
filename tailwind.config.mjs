a375ee1722d2146b286afaefb2d984b23f1b2627
nsyifa <nasywa.syifa.azizah@mail.ugm.ac.id>
2025-01-24 22:31:20 +0700

    feat: add colors and animation

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -13,11 +13,26 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
         "light-beige": "#FFF7E4",
+        "sakbar-light": "#FCFBF4",
         "sakbar-brown": "#392413",
+        "sakbar-mustard": "#C87E15",
+        "sakbar-orange": "#F5A623",
+        "sakbar-green": "#4F7942",
       },
       fontFamily: {
         playfair: ["var(--font-playfair-display)", "serif"],
         koho: ["var(--font-koho)", "sans-serif"],
+      },
+      keyframes: {
+        slideIn: {
+          "0%": { transform: "translateX(-100%)", opacity: "0" },
+
+          "100%": { transform: "translateX(0)", opacity: "1" },
+        },
+      },
+
+      animation: {
+        slideIn: "slideIn 0.5s ease-out forwards", // Adjust duration and easing as needed
       },
     },
   },
